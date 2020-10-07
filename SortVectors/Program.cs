634d54a25fa680bd759e15e7486a158e83787c30
Ivan Todorov Vasilev <IVAN TODOROV.VASILEV@local.ittcesena.it>
2020-10-07 11:07:22 +0200

    first step

---
+++ SortVectors/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SortVectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vett = new int[10] { 4, -1, 100, -85, 4, 6, 77, -35, 2, 0 };
+
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine(vett[i]);
+        }
+    }
+}
